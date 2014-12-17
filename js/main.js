370872d922d7d5f9299b2e9e7b9b791556402cb4
David Simon <me@davidsimon.ch>
2014-12-17 13:49:56 +0100

    [Feature] Glitches 

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 ---
 {% include components/jquery/jquery-1.11.1.min.js %}
 {% include components/isotope/isotope.pkgd.min.js %}
+{% include components/glitch/glitch.jquery.js %}
+{% include components/html2canvas/html2canvas.js %}
+{% include components/glitch/glitch.js %}
 
 
 (function ($) {
@@ -10,5 +13,25 @@
         transitionDuration: 0,
         layoutMode: 'fitRows'
     });
+
+
+    $('[data-js-glitch]').glitch({
+        bg: null,    // background color
+        maxint: 10,     // max interval between glitchings
+        minint: 2,      // min interval between glitchings
+        maxglitch: 5,   // max number of twitches
+        hshift: 3,      // max horizontal shift
+        vshift: 3,      // max vertical shift
+        direction: 'random' // 'horizontal', 'vertical' or 'random'
+    });
+
+    // $('body').each(function () {
+    //     var self = this;
+    //     setTimeout(function () {
+    //         $(self).glitcher('replace', {
+    //             amount: 8
+    //         });
+    //     },2500);
+    // });
 
 }(jQuery));
